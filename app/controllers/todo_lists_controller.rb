2230a4d0f9bdc3c656a39ca206afb6f51e50deed
the-nv <n.verma9252@gmail.com>
2020-09-09 14:50:24 +0530

    added update feature to todo-list

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -10,6 +10,10 @@ class TodoListsController < ApplicationController
         @todo_list = TodoList.new
     end
 
+    def edit
+        @todo_list = TodoList.find(params[:id])
+    end
+
     def create
         @todo_list = TodoList.new(todo_list_params)
 
@@ -17,6 +21,16 @@ class TodoListsController < ApplicationController
             redirect_to @todo_list
         else
             render 'new'
+        end
+    end
+
+    def update
+        @todo_list = TodoList.find(params[:id])
+
+        if @todo_list.update(todo_list_params)
+            redirect_to @todo_list
+        else
+            render 'edit'
         end
     end
 
