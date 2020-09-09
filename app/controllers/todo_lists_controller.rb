27de81653a2fa658908e064a8ce19e4d8a89b608
the-nv <n.verma9252@gmail.com>
2020-09-09 15:21:44 +0530

    added delete feature to todo-list

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -34,6 +34,14 @@ class TodoListsController < ApplicationController
         end
     end
 
+    def destroy
+        @todo_list =TodoList.find(params[:id])
+
+        @todo_list.destroy
+
+        redirect_to todo_lists_path
+    end
+
     private
         def todo_list_params
             params.require(:todo_list).permit(:title, :description)
